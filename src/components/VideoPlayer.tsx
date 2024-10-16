e11a77ecfb8da2e95bfb1ae1a185652408c8d0ff
Vinicius Moura Rebelo <vinicius.rebelo@meta.com.br>
2024-10-16 10:00:22 -0300

    xcxcxcxcxc

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -6,7 +6,7 @@ import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 import { pxToRem } from "../theme/typography";
 import useResponsive from "../hooks/useResponsive";
-import Thumb from "assets/thumbVideo.png"
+//import Thumb from "assets/thumbVideo.png"
 
 export function VideoPlayer({ videoUrl }: { videoUrl: string }) {
   const isMobile = useResponsive("down", "sm");
@@ -94,7 +94,7 @@ export function VideoPlayer({ videoUrl }: { videoUrl: string }) {
       }}
       onClick={togglePlayPause}
 
-      poster={Thumb}
+      poster="assets/thumbVideo.png"
     >
       <source src={videoUrl} type="video/mp4" />
       Seu navegador não suporta a reprodução deste vídeo.
